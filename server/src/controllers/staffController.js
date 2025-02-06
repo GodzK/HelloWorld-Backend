3ab5f89ccd0c5a61ec9958249ba0d7fd029dde08
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-02-06 12:56:23 +0700

    add for work with api

--- server/src/controllers/staffController.js
+++ server/src/controllers/staffController.js
@@ -2,8 +2,9 @@ import { getAllStaff } from "../models/staffModel.js";
 
 export const fetchStaff = async (req, res) => {
     try {
-        const staff = await getAllStaff();d
+        const staff = await getAllStaff();
         res.status(200).json({ staff });
+        console.log(staff)
     } catch (err) {
         res.status(500).json({ message: "Failed to fetch staff", error: err.message });
     }
--- server/src/routes/staffRoute.js
+++ server/src/routes/staffRoute.js
@@ -3,6 +3,6 @@ import { fetchStaff } from "../controllers/staffController.js";
 import { verifyToken, checkRole } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
-router.get("/", verifyToken, checkRole("admin"), fetchStaff);
+router.get("/", verifyToken, checkRole("staff"), fetchStaff);
 
 export default router;
